92c0fbc29dfa02ddc2233a6b1f61229f5675b786
Phuoc Anh Kha Le <76896393+anhkha2003@users.noreply.github.com>
2024-02-02 22:23:17 -0600

    Add second layout

---
+++ functions.js
@@ -0,0 +1,29 @@
+function toggleStyleSheet() {
+    var styleElement = document.getElementById('mainStyleSheet');
+    var currentStyle = localStorage.getItem('mainStyleSheet');
+    
+    if (currentStyle == null || currentStyle == "secondStyle.css") {
+        currentStyle = "firstStyle.css";
+    }
+    else {
+        currentStyle = "secondStyle.css";
+    }
+    styleElement.setAttribute('href', currentStyle);
+    localStorage.setItem('mainStyleSheet', currentStyle);
+}
+
+function loadStyleSheet() {
+    var styleElement = document.getElementById('mainStyleSheet');
+    var currentStyle = localStorage.getItem('mainStyleSheet');
+    if (currentStyle == null) {
+        currentStyle = "firstStyle.css";
+    }
+    styleElement.setAttribute('href', currentStyle);
+
+    var toggleSwitch = document.getElementById('checkbox');
+    toggleSwitch.checked = (currentStyle === "secondStyle.css");
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    loadStyleSheet();
+});
